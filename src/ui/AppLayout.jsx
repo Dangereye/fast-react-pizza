b37c70ee4e8cd007b6676d49b7586bd5f0ffd85d
Craig Puxty <craig_puxty@talktalk.net>
2024-01-03 13:50:11 +0000

    Added Loader, updated AppLayout.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
+// React router
+import { Outlet, useNavigation } from 'react-router-dom';
+
+// Components
+import Loader from './Loader';
 import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
-import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
   return (
-    <div>
+    <div className='layout'>
+      {isLoading && <Loader />}
       <Header />
       <main>
         <h1>Content</h1>
