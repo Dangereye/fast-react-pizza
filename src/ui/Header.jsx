d2811d17d0399bed0c3326eb532210a550f08444
Craig Puxty <craig_puxty@talktalk.net>
2024-01-04 17:52:36 +0000

    Styling - buttons, element states and transitions.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -7,7 +7,7 @@ import Username from '../features/user/Username';
 
 export default function Header() {
   return (
-    <header className='flex items-center justify-between px-4 py-3 uppercase bg-yellow-500 border-b border-stoe-200 sm:px-6'>
+    <header className='flex items-center justify-between px-4 py-3 uppercase bg-yellow-400 border-b border-stoe-200 sm:px-6'>
       <Link to='/' className='tracking-widest'>
         Fast React Pizza Co.
       </Link>
