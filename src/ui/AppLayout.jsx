e651e32f8d15d14a5bdee16f5c512956622ff37d
Craig Puxty <craig_puxty@talktalk.net>
2024-01-05 21:10:43 +0000

    Updated Loader styling.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -12,6 +12,7 @@ export default function AppLayout() {
   return (
     <div className='grid grid-rows-[auto_1fr_auto] h-screen'>
       {isLoading && <Loader />}
+
       <Header />
       <div className='overflow-y-scroll'>
         <main className='max-w-3xl mx-auto'>
