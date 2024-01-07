e0f3c5b95c9abb314d0e715592fc5c7fb2ddd4b3
Craig Puxty <craig_puxty@talktalk.net>
2024-01-07 13:16:20 +0000

    Styled CreateOrder. Updated Buttons styles. Updated title, metatags & favicon.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Button({ disabled, children, to, type }) {
   const baseStyles =
-    'inline-block font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+    'inline-block text-sm font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
   const styles = {
     primary: `${baseStyles} px-4 py-3 md:px-6 md:py-4`,
     secondary:
-      'inline-block font-semibold tracking-wide uppercase transition-colors duration-300 border-2 border-stone-300 rounded-full text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
+      'inline-block text-sm font-semibold tracking-wide uppercase transition-colors duration-300 border-2 border-stone-300 rounded-full text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
     small: `${baseStyles} px-4 py-2 md:px-5 md:py-2.5 text-xs`,
   };
 
